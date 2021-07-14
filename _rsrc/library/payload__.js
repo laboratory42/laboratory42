6e618f5b07932f02ab10eb89a2b96a556d1c1cdc
Zsolt Sándor <ezsolt@gmail.com>
2021-07-14 09:28:40 +0200

    typescript fetch // not working yet

--- _rsrc/library/payload__.js
+++ _rsrc/library/payload__.js
@@ -4,20 +4,34 @@
  * fetch json data or html partial from url or from local file
  *
  */
-var Payload = /** @class */ (function () {
-    function Payload(filename) {
+// https://basarat.gitbook.io/typescript/future-javascript/promise
+class Payload {
+    // const fetch = require("node-fetch");
+    constructor(filename) {
         this.__json = filename;
     }
-    Payload.prototype.fetchJson = function () {
+    fetchJson() {
         console.log(this.__json);
-        // one.then(value => {
-        //     console.log('resolved', value);
-        // });
-        // one.catch(error => {
-        //     console.log('rejected', error);
-        // });
-    };
-    return Payload;
-}());
-var json = new Payload('json.file');
+        const one = new Promise((resolve, reject) => {
+            const response = fetch(this.__json);
+            console.log(response);
+            // fetch(this.__json).then(function (response) {
+            // if (response.status == 200) {
+            //     return response.json();
+            // } else {
+            //     return Promise.reject(response);
+            // }
+            // })
+        }); // end promise
+        one.then(value => {
+            console.log('resolved', value);
+        });
+        one.catch(error => {
+            console.log('rejected', error);
+        });
+    }
+}
+// let json = new Payload('json.file');
+// json.fetchJson();
+let json = new Payload('payload.json');
 json.fetchJson();
--- _rsrc/library/payload__.ts
+++ _rsrc/library/payload__.ts
@@ -8,6 +8,10 @@
 class Payload {
 
     private __json: string;
+    result: string;
+    number: number;
+
+    // const fetch = require("node-fetch");
 
     constructor(filename: string) {
         this.__json = filename
@@ -16,22 +20,65 @@ class Payload {
     fetchJson() {
 
         console.log(this.__json);
-        // one.then(value => {
-        //     console.log('resolved', value);
-        // });
-        // one.catch(error => {
-        //     console.log('rejected', error);
-        // });
+
+        const one = new Promise<string>((resolve, reject) => {
+
+            const response = fetch(this.__json);
+
+            console.log(response);
+            // fetch(this.__json).then(function (response) {
+            // if (response.status == 200) {
+            //     return response.json();
+            // } else {
+            //     return Promise.reject(response);
+            // }
+            // })
+
+        }); // end promise
+
+        one.then(value => {
+            console.log('resolved', value);
+        });
+        one.catch(error => {
+            console.log('rejected', error);
+        });
 
     }
 
 
 
 
+
+
+    // get json response
+    // fetchJson() {
+    //     return new Promise((resolve, reject) => {
+    //         fetch(this.__json).then(function (response) {
+    //             if (response.status == 200) {
+    //                 return response.json();
+    //             } else {
+    //                 return Promise.reject(response);
+    //             }
+    //         })
+    //             .then(result => {
+    //                 resolve(result);
+    //             })
+    //             .catch(function (e) {
+    //                 console.warn('Sorry, there is an error.', e);
+    //             })
+    //     })
+    // }
+
+
+
 }
 
-let json = new Payload('json.file');
+// let json = new Payload('json.file');
+// json.fetchJson();
+let json = new Payload('payload.json');
 json.fetchJson();
+
+
 
 
     // https://blog.bitsrc.io/keep-your-promises-in-typescript-using-async-await-7bdc57041308
