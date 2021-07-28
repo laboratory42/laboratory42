905e6b21e68ab4894e37eafe31c169fdd32bfe74
Zsolt Sándor <ezsolt@gmail.com>
2021-07-28 22:12:11 +0200

    Update payload.js

--- _rsrc/library/payload.js
+++ _rsrc/library/payload.js
@@ -13,13 +13,14 @@ class Payload {
     // get json response
     fetchJson() {
         return new Promise((resolve, reject) => {
-            fetch(this.__data).then(function (response) {
-                if (response.status == 200) {
-                    return response.json();
-                } else {
-                    return Promise.reject(response);
-                }
-            })
+            fetch(this.__data)
+                .then(function (response) {
+                    if (response.status == 200) {
+                        return response.json();
+                    } else {
+                        return Promise.reject(response);
+                    }
+                })
                 .then(result => {
                     resolve(result);
                 })
@@ -29,23 +30,24 @@ class Payload {
                 })
         })
     }
+    // fetchJson
 
 
 
     // get text response
     fetchHtml(partial) {
         return new Promise((resolve, reject) => {
-            fetch(this.__data).then(function (response) {
+            fetch(this.__data)
+                .then(function (response) {
 
-                // ? https://github.com/github/fetch
-                credentials: 'same-origin'
+                    credentials: 'same-origin' // ? https://github.com/github/fetch
 
-                if (response.status == 200) {
-                    return response.text();
-                } else {
-                    return Promise.reject(response);
-                }
-            })
+                    if (response.status == 200) {
+                        return response.text();
+                    } else {
+                        return Promise.reject(response);
+                    }
+                })
                 .then(result => {
                     resolve(result);
                 })
@@ -54,20 +56,6 @@ class Payload {
                 })
         })
     }
-
-
-
-    // fetchPartial(partial, target) {
-    //     fetch(partial).then(function (response) {
-    //         return response.text()
-    //     }).then((html) => {
-    //         let elem = document.querySelector(target)
-    //         elem.innerHTML = html
-    //     }).catch(function (e) {
-    //         // console.log('Fetch problem: ' + e.message);
-    //         console.warn('Something went wrong.', e)
-    //     })
-    // }
 
 
 }
